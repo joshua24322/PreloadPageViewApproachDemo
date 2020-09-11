b648cdf825dc882e5fd83333cb56d69776a8453a
Joshua Chang <joshua24322@gmail.com>
2020-09-11 23:19:00 +0800

    Initial Commit

---
+++ PreloadPageViewApproachDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PreloadPageViewApproachDemo
+//
+//  Created by 金融研發一部-張書彬 on 2020/9/11.
+//  Copyright © 2020 Joshua Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
